e677a3fb617186abcb288cb8e172af99029e5c33
Angy1208 <ang.floresv@gmail.com>
2024-01-24 13:44:55 -0600

    Se agrego about y maquetacion

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-#repositorio
-#repositorio
+#React + Vite
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,38 @@
+const About = () => {
+    return (
+        <section>
+            <article>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </article>
+            <aside>
+                <h1>
+                    Angel Flores
+                </h1>
+                <p>
+                    <b>A passionate frontend developer from México</b>
+                </p>
+                <ul>
+                    <li>Lorem ipsum dolor sit</li>
+                    <li>Lorem ipsum dolor sit</li>
+                </ul>
+                <p>
+                    <b>Connect with me</b>
+                </p>
+                <p>Linkedin</p>
+                <p>Github</p>
+
+                <section>
+                    <p>Languages and Tools</p>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                    </ul>
+                </section>
+            </aside>
+        </section>
+    )
+};
+
+export default About;
